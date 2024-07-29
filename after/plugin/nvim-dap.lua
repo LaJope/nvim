5468c77199aa5c675d36cf0b3f07c0b42c9d1a7a
LaJope <Siephan2.0.0.3@gmail.com>
2024-07-29 23:36:56 +0300

    New

--- after/plugin/nvim-dap.lua
+++ after/plugin/nvim-dap.lua
@@ -18,61 +18,16 @@ dap.adapters.codelldb = {
   }
 }
 
-function Scandir(directory)
-  local ind = 0
-  local exe_paths = {}
-  local popen = io.popen
-
-  local proj_dirs = popen('ls -1 "' .. directory .. '" 2> /dev/null')
-  if (proj_dirs == nil) then
-    do
-      ind = ind + 1
-      exe_paths[ind] = 0
-      return exe_paths
-    end
-  end
-
-  for dir in proj_dirs:lines() do
-    local proj_execs = popen('ls -1 "' .. directory .. '/' .. dir .. '" 2> /dev/null')
-    if (proj_execs == nil) then
-      do
-        ind = ind + 1
-        exe_paths[ind] = 1
-        return exe_paths
-      end
-    end
-
-    for exec in proj_execs:lines() do
-      if (dir == exec) then
-        exe_paths[dir] = exec
-      end
-    end
-    proj_execs:close()
-  end
-  proj_dirs:close()
-
-  return exe_paths
-end
-
 dap.configurations.cpp = {
   {
     name = "Launch executable",
     type = "codelldb",
     request = "launch",
     program = function()
-      local dir = vim.fn.getcwd() .. '/out/Debug/'
-      -- local execs = Scandir(dir)
-      -- for ind, item in ipairs(execs) do
-      --   vim.print(ind .. ' ' .. item)
-      -- end
-      -- local input = tonumber(vim.fn.input('Choose executable: '))
-      local input = vim.fn.input('Enter project name: ')
-      -- return dir .. execs[input] .. '/' .. execs[input]
-      return dir..input..'/'..input
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/out", "file")
     end,
     cwd = "${workspaceFolder}",
     terminal = 'integrated',
-    -- sourceLanguages = { 'c++' },
     stopOnEntry = false,
     showDisassembly = "never",
   }
