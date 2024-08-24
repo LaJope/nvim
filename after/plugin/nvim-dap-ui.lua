6db6554ee29bbd1dbd7bdca9917ff5291d81d02c
LaJope <Siephan2.0.0.3@gmail.com>
2024-08-24 15:05:12 +0300

    New

--- after/plugin/nvim-dap-ui.lua
+++ after/plugin/nvim-dap-ui.lua
@@ -1,6 +1,45 @@
 local dap = require('dap')
 local dapui = require('dapui')
-dapui.setup()
+dapui.setup({
+  layouts = {
+    {
+      elements = {
+        {
+          id = "scopes",
+          size = 0.5
+        },
+        {
+          id = "breakpoints",
+          size = 0.25
+        },
+        {
+          id = "stacks",
+          size = 0.25
+        },
+        -- {
+        --   id = "watches",
+        --   size = 0.25
+        -- }
+      },
+      position = "left",
+      size = 70
+    },
+    {
+      elements = {
+        {
+          id = "repl",
+          size = 0.5
+        },
+        {
+          id = "console",
+          size = 0.5
+        }
+      },
+      position = "bottom",
+      size = 10
+    }
+  }
+})
 
 dap.listeners.after.event_initialized['dapui_config'] = function()
   dapui.open()
