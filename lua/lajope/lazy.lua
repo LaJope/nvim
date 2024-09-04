7b04f9c9b9811b940c763a1ea0d10c9844d928a0
LaJope <Siephan2.0.0.3@gmail.com>
2024-09-04 13:55:49 +0300

    New

--- lua/lajope/lazy.lua
+++ lua/lajope/lazy.lua
@@ -161,6 +161,23 @@ local plugins = {
   },
 
   'christoomey/vim-tmux-navigator',
+
+  -- install with yarn or npm
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+  {
+    "ellisonleao/glow.nvim",
+    config = true,
+    cmd = "Glow"
+  },
+
 }
 
 local opts = {}
